e470aa0a8da58dba984d67324d020629f0b0415f
Centre Team <cwma.campbell@gmail.com>
2022-03-31 14:59:11 +0100

    Add files via upload
    first upload

---
+++ kodi_file_format_rename.sh
@@ -0,0 +1,19 @@
+#########################################################################
+# rename files downloaded that are long and can be split on a delimiter #
+#########################################################################
+
+# e.g. file1 1972 S01E01-xxx.mp4
+# required output; file1 (1972) S01E01.mp4
+# note the parenthesis around year in output
+
+#!/bin/bash
+
+for i in *.mp4
+do
+
+# tr -d \' removes any quotes that may srround file names
+# sed wraps parenthesis around the date in the file name
+
+new_filename=$(echo "${i}" | cut -d "-" -f1 | tr -d \' | sed 's/1972/(1972)/')".mp4"
+cp "${i}" "${new_filename}"
+done
